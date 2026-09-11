bb2bea350387034f8213b4946b713f6e679b2e80
ealex <ealex@LAPTOP-4IV06LKD>
2026-09-11 12:44:40 -0500

    Agregar archivos de proyecto.

---
+++ JuegoDeCraps/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JuegoDeCraps;
+
+namespace JuegoDeCraps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al juego de Craps!");
+
+            Craps juego = new Craps();
+            juego.Jugar();
+        }
+    }
+}
